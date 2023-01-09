5b508e889825066680e94a2de2173bc21d8879d9
d4niel <olayajuandaniel8@gmail.com>
2023-01-09 11:03:51 -0500

    task model

---
+++ Example_API/Models/Tarea.cs
@@ -0,0 +1,15 @@
+﻿namespace Example_API.Models
+{
+    public class Tarea
+    {
+        public int ?id { get; set; }
+
+        public string title { get; set; }   
+
+
+        public string description { get; set; }
+
+
+        public char done { get; set; }
+    }
+}

1e2dea41d4abaa26eba00e6d64814faaa07b5e24
d4niel <olayajuandaniel8@gmail.com>
2023-01-09 10:58:23 -0500

    Agregar archivos de proyecto.

---
+++ Example_API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example_API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("getall")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+
+        [HttpGet("show/{id}")]
+        
+        public IActionResult getByid(int id)
+        {
+            return Ok("Esto es una cadena de texto " + id);
+        }
+
+
+        
+    }
+}

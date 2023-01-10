56b902587e1c2ea6ed6a702a234eeac987e12566
d4niel <olayajuandaniel8@gmail.com>
2023-01-10 15:25:58 -0500

    interfaces

---
+++ Example_API/Interfaces/ITarea.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Example_API.Interfaces
+{
+    public interface ITarea
+    {
+        public int ?id { get; set; }
+
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        public string done { get; set; }
+    }
+}
